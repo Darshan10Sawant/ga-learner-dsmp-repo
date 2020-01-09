c3a2d4952953d59ce4f25b825d129f7c8f57ebe6
Darshan10Sawant <Darshan10Sawant@users.noreply.github.com>
2020-01-10 00:52:11 +0530

    Code file created.

---
+++ Make-Sense-of-Census(MANIPULATING-DATA-WITH-NUMPY)/code.py
@@ -0,0 +1,91 @@
+# --------------
+# Importing header files
+import numpy as np
+
+# Path of the file has been stored in variable called 'path'
+data =np.genfromtxt(path, delimiter=",",skip_header=1) 
+#New record
+new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
+census=np.concatenate([new_record,data])
+#Code starts here
+
+
+
+# --------------
+#Code starts here
+age = census[:,0]
+max_age = max(age)
+min_age = min(age)
+age_mean = np.mean(age)
+age_std = np.std(age)
+
+
+# --------------
+#Code starts here
+race_0 =census[census[:,2]==0]
+race_1 =census[census[:,2]==1]
+race_2 =census[census[:,2]==2]
+race_3 =census[census[:,2]==3]
+race_4 =census[census[:,2]==4]
+len_0 = len(race_0)
+len_1 = len(race_1)
+len_2 = len(race_2)
+len_3 = len(race_3)
+len_4 = len(race_4)
+a = min(len_0,len_1,len_2,len_3,len_4)
+def min_value(a):
+    if a==len_0:
+        return 0
+    elif a==len_1:
+        return 1
+    elif a==len_2:
+        return 2
+    elif a==len_3:
+        return 3
+    else:
+        return 4
+
+minority_race = min_value(a)
+print(minority_race)
+
+
+
+# --------------
+#Code starts here
+"""def senior(x):
+    return x>60
+
+senior_citizens = list(filter(senior,census[:,0]))
+
+
+working_hours_sum =senior_citizens.sum(axis=0)[6]
+
+senior_citizens_len = len(senior_citizens)
+
+avg_working_hours = working_hours_sum/senior_citizens_len
+
+print((avg_working_hours))"""
+senior_citizens=census[census[:,0]>60]
+working_hours_sum=senior_citizens.sum(axis=0)[6]
+senior_citizens_len=len(senior_citizens)
+avg_working_hours=working_hours_sum/senior_citizens_len
+print((avg_working_hours))
+
+    
+
+
+
+# --------------
+#Code starts here
+high = census[census[:,1]>10]
+
+low = census[census[:,1]<=10]
+
+
+avg_pay_high = high[:,7].mean()
+print(avg_pay_high)
+
+avg_pay_low=low[:,7].mean()
+print(round(avg_pay_low,2))
+
+
